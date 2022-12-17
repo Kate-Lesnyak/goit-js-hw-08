c8589d6e10b530b9f8b7a513c2a0c14cffde917f
Kate-Lesnyak <catharinalesnyak@gmail.com>
2022-12-17 22:56:58 +0200

    add 2 tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+console.log(SimpleLightbox);
 console.log(galleryItems);
+
+const galleryImages = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryImages(galleryItems);
+
+galleryImages.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+function createGalleryImages(galleryItems) {
+  return galleryItems.map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+			<img class="gallery__image" src="${preview}" alt="${description}"/>
+			</a>`).join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+// Ознакомься с документацией библиотеки Vimeo плеера.
+// Добавь библиотеку как зависимость проекта через npm.
+// Инициализируй плеер в файле скрипта как это описано в секции pre - existing player, но учти что у тебя плеер добавлен как npm пакет, а не через CDN.
+// Разбери документацию метода on() и начни отслеживать событие timeupdate - обновление времени воспроизведения.
+// Сохраняй время воспроизведения в локальное хранилище.Пусть ключом для хранилища будет строка "videoplayer-current-time".
+// При перезагрузке страницы воспользуйся методом setCurrentTime() для того чтобы возобновить воспроизведение с сохраненной позиции.
+// Добавь в проект библиотеку lodash.throttle и сделай так, чтобы время воспроизведения обновлялось в хранилище не чаще чем раз в секунду.
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const PLAYBACK_TIME = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(currentTime, 1000));
+
+function currentTime(e) {
+  localStorage.setItem(PLAYBACK_TIME, JSON.stringify(e.seconds));
+}
+
+try {
+  player.setCurrentTime(JSON.parse(localStorage.getItem(PLAYBACK_TIME)));
+}
+catch (error) {
+  player.setCurrentTime(0);
+}
+
+// player.setCurrentTime(JSON.parse(localStorage.getItem(PLAYBACK_TIME) || 0));
